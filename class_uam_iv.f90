8854e446044c84495e47c231e1cda659e622e50c
Pablo Garcia <pablogar@barney.ce.cmu.edu>
2016-09-07 20:57:58 -0400

    Methods for file opening and header reading on combine_point_emis.f90 * Improved sytax on public variable declaration * New methods - open_file: opens the file in the assigned unit - read_header: Reads the file header, both file and region definition

--- class_uam_iv.f90
+++ class_uam_iv.f90
@@ -6,12 +6,15 @@ MODULE class_ptfile
 IMPLICIT NONE
 
 	PRIVATE
-	PUBLIC UAM_IV
+
+! 	Public variables
+	PUBLIC :: UAM_IV
 
 !	UAM-IV											! Input files
 	TYPE :: UAM_IV
+
 		CHARACTER(LEN=256) :: in_file				! Input filename
-		INTEGER :: file_unit						! Input unit
+		INTEGER :: unit								! Input unit
 		CHARACTER(LEN=4), DIMENSION(10) :: fname	! Name array
 		CHARACTER(LEN=4), DIMENSION(60) :: note		! Note array
 		INTEGER :: nseg,nspec,idate,jdate			! Species number and dates
@@ -27,8 +30,46 @@ IMPLICIT NONE
 		CHARACTER(LEN=10) :: ftype					! File type
 		LOGICAL lhdrspec, l3d, lbndry, lptsrc		! Filetype logicals
 ! 		l3d may not be necessary
+
 	END TYPE UAM_IV
 
+! 	Public methods
+	PUBLIC :: open_file, read_header
+
 CONTAINS
+
+	SUBROUTINE open_file(pt)
+
+		TYPE(UAM_IV), INTENT(IN) :: pt
+
+		OPEN (pt%unit,FILE=TRIM(pt%in_file),FORM='UNFORMATTED',STATUS='old')
+		WRITE(*,*) 'Opened file: ',TRIM(pt%in_file)
+
+	END SUBROUTINE open_file
+
+	SUBROUTINE read_header(pt)
+
+		TYPE(UAM_IV), INTENT(INOUT) :: pt
+		INTEGER :: i
+
+		READ (pt%unit) pt%fname,pt%note,pt%nseg,pt%nspec,pt%idate,pt%begtim,pt%jdate,&
+			&pt%endtim
+		WRITE(*,900) pt%fname,pt%note,pt%nseg,pt%nspec,pt%idate,pt%begtim,pt%jdate,&
+			&pt%endtim
+		WRITE(pt%ftype,'(10a1)') (pt%fname(i),i=1,10)
+		WRITE(*,*) 'File type is ',pt%ftype
+
+		READ (pt%unit) pt%orgx,pt%orgy,pt%iutm,pt%utmx,pt%utmy,pt%dx,pt%dy,pt%nx,pt%ny,pt%nz,&
+			&pt%nzlo,pt%nzup,pt%hts,pt%htl,pt%htu
+		WRITE(*,901) pt%orgx,pt%orgy,pt%iutm,pt%utmx,pt%utmy,pt%dx,pt%dy,pt%nx,pt%ny,pt%nz,&
+			&pt%nzlo,pt%nzup,pt%hts,pt%htl,pt%htu
+		READ (pt%unit) pt%i1,pt%j1,pt%nx1,pt%ny1
+
+!		Format strings
+
+900  	FORMAT(10a1,60a1,/,i2,1x,i3,1x,i6,f6.0,i6,f6.0)
+901  	FORMAT(2(f16.5,1x),i3,1x,4(f16.5,1x),5i4,3f7.0)
+
+	END SUBROUTINE read_header
 
 END MODULE
