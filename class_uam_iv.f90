bbf02169d7f8b40e9c28455fda177fe49083cf0d
Pablo Garcia <pablogar@barney.ce.cmu.edu>
2016-09-07 21:00:29 -0400

    Species Processing * on read_header   - Changed format strings from labels to char vars * Added read_species subroutine   - As in camx2csv * Added species list processing   - Gets unique spec names   - Finds the index for each unique in each file

--- class_uam_iv.f90
+++ class_uam_iv.f90
@@ -34,7 +34,7 @@ IMPLICIT NONE
 	END TYPE UAM_IV
 
 ! 	Public methods
-	PUBLIC :: open_file, read_header
+	PUBLIC :: open_file, read_header, read_species
 
 CONTAINS
 
@@ -51,25 +51,38 @@ CONTAINS
 
 		TYPE(UAM_IV), INTENT(INOUT) :: pt
 		INTEGER :: i
+		CHARACTER(LEN=41) :: h1format, h2format
+
+		h1format='(10a1,60a1,/,i2,1x,i3,1x,i6,f6.0,i6,f6.0)'
+		h2format='(2(f16.5,1x),i3,1x,4(f16.5,1x),5i4,3f7.0)'
 
 		READ (pt%unit) pt%fname,pt%note,pt%nseg,pt%nspec,pt%idate,pt%begtim,pt%jdate,&
 			&pt%endtim
-		WRITE(*,900) pt%fname,pt%note,pt%nseg,pt%nspec,pt%idate,pt%begtim,pt%jdate,&
+		WRITE(*,h1format) pt%fname,pt%note,pt%nseg,pt%nspec,pt%idate,pt%begtim,pt%jdate,&
 			&pt%endtim
 		WRITE(pt%ftype,'(10a1)') (pt%fname(i),i=1,10)
 		WRITE(*,*) 'File type is ',pt%ftype
 
 		READ (pt%unit) pt%orgx,pt%orgy,pt%iutm,pt%utmx,pt%utmy,pt%dx,pt%dy,pt%nx,pt%ny,pt%nz,&
 			&pt%nzlo,pt%nzup,pt%hts,pt%htl,pt%htu
-		WRITE(*,901) pt%orgx,pt%orgy,pt%iutm,pt%utmx,pt%utmy,pt%dx,pt%dy,pt%nx,pt%ny,pt%nz,&
+		WRITE(*,h2format) pt%orgx,pt%orgy,pt%iutm,pt%utmx,pt%utmy,pt%dx,pt%dy,pt%nx,pt%ny,pt%nz,&
 			&pt%nzlo,pt%nzup,pt%hts,pt%htl,pt%htu
 		READ (pt%unit) pt%i1,pt%j1,pt%nx1,pt%ny1
 
-!		Format strings
-
-900  	FORMAT(10a1,60a1,/,i2,1x,i3,1x,i6,f6.0,i6,f6.0)
-901  	FORMAT(2(f16.5,1x),i3,1x,4(f16.5,1x),5i4,3f7.0)
-
 	END SUBROUTINE read_header
+
+	SUBROUTINE read_species(pt)
+
+		TYPE(UAM_IV), INTENT(INOUT) :: pt
+		INTEGER :: i,j
+
+		ALLOCATE(pt%spname(10,pt%nspec))
+		ALLOCATE(pt%c_spname(pt%nspec))
+		READ (pt%unit) ((pt%spname(i,j),i=1,10),j=1,pt%nspec)
+		WRITE(pt%c_spname,'(10a1)') ((pt%spname(i,j),i=1,10),j=1,pt%nspec)
+		WRITE(*,*) pt%c_spname
+! 		WRITE(*,'(10a1)') ((pt%spname(i,j),i=1,10),j=1,pt%nspec)
+
+	END SUBROUTINE read_species
 
 END MODULE
